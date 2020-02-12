934308261831dffcd1847f9dcb6c43580fb975ee
Nicholas Dongoske <nickdongoske@gmail.com>
2020-02-12 22:47:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,12 +3,11 @@ require 'pry'
 def map(array)
  i = 0
  new_array = []
-while i < new_array do
+while i < array do
   new_array.push(yield(array[i]))
-  
   i += 1
 end
-binding.pry
+
 new_array
 end
 
